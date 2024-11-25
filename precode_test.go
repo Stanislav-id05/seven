765de675603279508789fa139070b348e7a81794
Stanislav-id05 <fiddles.wing0v@icloud.com>
2024-11-25 20:21:59 +0400

    Add files via upload

---
+++ precode_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil) // здесь нужно создать запрос к сервису
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	// здесь нужно добавить необходимые проверки
+
+	if status := responseRecorder.Code; status != http.StatusOK {
+		assert.Equal(t, http.StatusOK, status)
+	}
+
+	body := responseRecorder.Body.String()
+	list := strings.Split(body, ",")
+
+	if len(list) != totalCount {
+		assert.Equal(t, totalCount, strings.Count(responseRecorder.Body.String(), ",")+1)
+	}
+}
+
+func TestMainHandlerCity(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=Sochi", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
+}
+
+func TestMainHandlerCorrectRequest(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=6&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, http.StatusOK, responseRecorder.Code)
+
+}
